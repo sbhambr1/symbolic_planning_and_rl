e2a33678c1199f6d6c6c30bdeb854f16739e2c80
GuanSuns <guansuns@outlook.com>
2021-04-08 23:08:59 -0700

    update

--- lin/SDRL_Project/learning_agents/planning_rl/sdrl/sdrl_agent.py
+++ lin/SDRL_Project/learning_agents/planning_rl/sdrl/sdrl_agent.py
@@ -74,9 +74,9 @@ class SDRL_Agent(Value_Based_Agent):
                     score += external_reward
 
                     intrinsic_reward, subgoal_done = self.env.get_intrinsic_reward(sub_goal)
-                    subgoal_score += intrinsic_reward
                     if subgoal_agent.episode_step + 1 >= self.hyper_params.max_goal_step:
                         intrinsic_reward -= 1
+                    subgoal_score += intrinsic_reward
 
                     # save the new transition
                     transition = (state, action, intrinsic_reward, next_state, subgoal_done, info)
